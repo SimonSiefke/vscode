07da4d7965c1b02641ef566e88da3e3e52249d95
Simon Siefke <simon.siefke@gmail.com>
2023-12-02 21:15:03 +0100

    dispose more disposables

--- src/vs/platform/list/browser/listService.ts
+++ src/vs/platform/list/browser/listService.ts
@@ -430,6 +430,7 @@ export class WorkbenchPagedList<T> extends PagedList<T> {
 		this.disposables.add(workbenchListOptionsDisposable);
 
 		this.contextKeyService = createScopedContextKeyService(contextKeyService, this);
+		this.disposables.add(this.contextKeyService);
 
 		this.disposables.add(createScrollObserver(this.contextKeyService, this.widget));
 
@@ -1211,6 +1212,7 @@ class WorkbenchTreeInternals<TInput, T, TFilterData> {
 		@IConfigurationService configurationService: IConfigurationService
 	) {
 		this.contextKeyService = createScopedContextKeyService(contextKeyService, tree);
+		this.disposables.push(this.contextKeyService)
 
 		this.disposables.push(createScrollObserver(this.contextKeyService, tree));
 
