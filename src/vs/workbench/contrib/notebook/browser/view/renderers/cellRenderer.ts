659b4afe1dd6843c2f8e55e4da8d100e85fdcc25
Simon Siefke <simon.siefke@gmail.com>
2024-02-19 23:24:16 +0100

    remove unused code

--- src/vs/workbench/contrib/notebook/browser/view/renderers/cellRenderer.ts
+++ src/vs/workbench/contrib/notebook/browser/view/renderers/cellRenderer.ts
@@ -376,7 +376,7 @@ export class CodeCellRenderer extends AbstractCellRenderer implements IListRende
 	}
 
 	disposeTemplate(templateData: CodeCellRenderTemplate): void {
-		templateData.editor = null!; // Strict null override - nulling out in dispose
+		// templateData.editor = null!; // Strict null override - nulling out in dispose
 		templateData.elementDisposables.dispose();
 		templateData.templateDisposables.dispose();
 	}
