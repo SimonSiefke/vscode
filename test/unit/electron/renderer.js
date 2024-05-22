07e5bf31da2aef16d0c493b53f7ee62618397655
Simon Siefke <simon.siefke@gmail.com>
2024-05-22 22:13:28 +0200

    fix test

--- test/unit/electron/renderer.js
+++ test/unit/electron/renderer.js
@@ -431,7 +431,7 @@ function runTests(opts) {
 	});
 }
 
-ipcRenderer.on('run', (e, opts) => {
+globalThis.setRun((e, opts) => {
 	initLoader(opts);
 	runTests(opts).catch(err => {
 		if (typeof err !== 'string') {
