ef3310469b70a3537cb3beb7a7e9da86dd927592
Simon Siefke <simon.siefke@gmail.com>
2024-06-06 09:37:48 +0200

    feature: add sample extension

---
+++ extensions/hello-error/src/main.ts
@@ -0,0 +1,13 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as vscode from 'vscode';
+
+
+export function activate(): void {
+	vscode.commands.registerCommand('hello-error.hello-error', () => {
+		throw new TypeError('x is not a function');
+	});
+}
