b1dd8954e88687c08fef72e003b2bbf9863f4f66
Simon Siefke <simon.siefke@gmail.com>
2025-11-09 22:13:32 +0100

    undo

--- src/vs/workbench/services/extensions/electron-browser/extensionsScannerService.ts
+++ src/vs/workbench/services/extensions/electron-browser/extensionsScannerService.ts
@@ -3,21 +3,6 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-<<<<<<< HEAD:src/vs/workbench/services/extensions/electron-sandbox/extensionsScannerService.ts
-import { URI } from 'vs/base/common/uri';
-import { IConfigurationService } from 'vs/platform/configuration/common/configuration.js';
-import { INativeEnvironmentService } from 'vs/platform/environment/common/environment';
-import { IExtensionsProfileScannerService } from 'vs/platform/extensionManagement/common/extensionsProfileScannerService';
-import { IExtensionsScannerService, NativeExtensionsScannerService, } from 'vs/platform/extensionManagement/common/extensionsScannerService';
-import { IFileService } from 'vs/platform/files/common/files';
-import { InstantiationType, registerSingleton } from 'vs/platform/instantiation/common/extensions';
-import { IInstantiationService } from 'vs/platform/instantiation/common/instantiation';
-import { ILogService } from 'vs/platform/log/common/log';
-import { IProductService } from 'vs/platform/product/common/productService';
-import { IUriIdentityService } from 'vs/platform/uriIdentity/common/uriIdentity';
-import { IUserDataProfilesService } from 'vs/platform/userDataProfile/common/userDataProfile';
-import { IUserDataProfileService } from 'vs/workbench/services/userDataProfile/common/userDataProfile';
-=======
 import { URI } from '../../../../base/common/uri.js';
 import { INativeEnvironmentService } from '../../../../platform/environment/common/environment.js';
 import { IExtensionsProfileScannerService } from '../../../../platform/extensionManagement/common/extensionsProfileScannerService.js';
@@ -30,7 +15,6 @@ import { IProductService } from '../../../../platform/product/common/productServ
 import { IUriIdentityService } from '../../../../platform/uriIdentity/common/uriIdentity.js';
 import { IUserDataProfilesService } from '../../../../platform/userDataProfile/common/userDataProfile.js';
 import { IUserDataProfileService } from '../../userDataProfile/common/userDataProfile.js';
->>>>>>> origin/main:src/vs/workbench/services/extensions/electron-browser/extensionsScannerService.ts
 
 export class ExtensionsScannerService extends NativeExtensionsScannerService implements IExtensionsScannerService {
 
@@ -44,14 +28,13 @@ export class ExtensionsScannerService extends NativeExtensionsScannerService imp
 		@IProductService productService: IProductService,
 		@IUriIdentityService uriIdentityService: IUriIdentityService,
 		@IInstantiationService instantiationService: IInstantiationService,
-		@IConfigurationService configurationService: IConfigurationService
 	) {
 		super(
 			URI.file(environmentService.builtinExtensionsPath),
 			URI.file(environmentService.extensionsPath),
 			environmentService.userHome,
 			userDataProfileService.currentProfile,
-			userDataProfilesService, extensionsProfileScannerService, fileService, logService, environmentService, productService, uriIdentityService, instantiationService, configurationService);
+			userDataProfilesService, extensionsProfileScannerService, fileService, logService, environmentService, productService, uriIdentityService, instantiationService);
 	}
 
 }
