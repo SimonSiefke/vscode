463bfc2d3612a0d96e78ddf2f508996258060f14
Simon Siefke <simon.siefke@gmail.com>
2026-08-28 21:29:53 +0000

    fix: reuse modal editor memento id

--- src/vs/workbench/browser/parts/editor/modalEditorPart.ts
+++ src/vs/workbench/browser/parts/editor/modalEditorPart.ts
@@ -873,8 +873,6 @@ interface IPosition {
 
 class ModalEditorPartImpl extends EditorPart implements IModalEditorPart {
 
-	private static COUNTER = 1;
-
 	private readonly _onWillClose = this._register(new Emitter<void>());
 	readonly onWillClose = this._onWillClose.event;
 
@@ -936,8 +934,7 @@ class ModalEditorPartImpl extends EditorPart implements IModalEditorPart {
 		@IHostService hostService: IHostService,
 		@IContextKeyService private readonly modalContextKeyService: IContextKeyService,
 	) {
-		const id = ModalEditorPartImpl.COUNTER++;
-		super(editorPartsView, `workbench.parts.modalEditor.${id}`, localize('modalEditorPart', "Modal Editor Area"), windowId, instantiationService, themeService, configurationService, storageService, layoutService, hostService, modalContextKeyService);
+		super(editorPartsView, `workbench.parts.modalEditor.${windowId}`, localize('modalEditorPart', "Modal Editor Area"), windowId, instantiationService, themeService, configurationService, storageService, layoutService, hostService, modalContextKeyService);
 
 		this._maximized = options?.maximized ?? false;
 		this._size = options?.size;
